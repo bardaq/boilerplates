842747b7461b9fe54fabbda5ffdb40458f6b464c
Dmitry Klymenko <dmitryklymenko@Dmitrys-Mac-mini.local>
2020-07-25 12:20:23 +0300

    cleanup

--- packages/shared/index.test.js
+++ packages/shared/index.test.js
@@ -3,7 +3,7 @@ import { sharedFunc } from './index';
 describe('Shared package tests', () => {
     it('Should sum/concat two argument', () => {
         expect(typeof sharedFunc).toBe('function');
-        const res = shared.sharedFunc(1, 2);
+        const res = sharedFunc(1, 2);
         expect(res).toBe(3);
     })
 })
