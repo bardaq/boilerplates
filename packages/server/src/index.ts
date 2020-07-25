e70f579d25aae097e950fc40bb28759e2f40aea5
Dmitry Klymenko <dmitryklymenko@Dmitrys-Mac-mini.local>
2020-07-25 13:29:44 +0300

    add a simple server

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -1,3 +1,11 @@
 import { sharedFunc } from '@monorepo/shared';
+import expess from 'express';
 
-console.log(`Server has access to the sharedFunc – ${sharedFunc}`);
+const app = expess();
+
+app.get('/', (req, res) => {
+    const something = sharedFunc(1, 2);
+    res.send('Hello World! ' + something);
+})
+
+app.listen(3000, () => console.log('Started on 3000'))
